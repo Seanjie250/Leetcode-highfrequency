d179117dc25a68cbf1e94b56a871a4eb15738343
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-15 13:24:59 +0800

    File updated at 5/15/2025, 1:24:59 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,4 +1,4 @@
-# Last updated: 5/15/2025, 1:18:39 PM
+# Last updated: 5/15/2025, 1:24:59 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,13 +6,15 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current = head
-        prev = None
-        while current:
-            tmp = current.next
-            current.next = prev
-            prev = current
-            current = tmp
-        return prev
+        return self.reverse(head,None)
+    def reverse(self,cur:ListNode,pre:ListNode) -> ListNode:
+        if cur == None:
+            return pre
+        tmp = cur.next
+        cur.next = pre
+        return self.reverse(tmp,cur)
+
+
+
 
         

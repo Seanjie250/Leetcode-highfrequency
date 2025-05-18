6d53d59f9969103ec67e3c4a64b7237cd0441060
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-18 09:51:04 +0800

    File updated at 5/18/2025, 9:51:03 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,11 +1,19 @@
-# Last updated: 5/17/2025, 4:58:17 PM
+# Last updated: 5/18/2025, 9:51:03 AM
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        record = dict()
-        for index , value in enumerate(nums):
-            if target - value in record:
-                return [index,record[target-value]]
-            record[value] = index
-        return []
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        hashmap = dict()
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 + n2 in hashmap:
+                    hashmap[n1 + n2] += 1
+                else:
+                    hashmap[n1 + n2] = 1
+        count = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                key = -n3 - n4
+                if key in hashmap:
+                    count += hashmap[key]
+        return count
 
         

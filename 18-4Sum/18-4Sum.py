af5e732378ad9e017250d3e581326f3aa1aa3df3
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-19 10:14:40 +0800

    File updated at 5/19/2025, 10:14:40 AM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,40 +1,12 @@
-# Last updated: 5/18/2025, 3:50:31 PM
+# Last updated: 5/19/2025, 10:14:40 AM
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        record = []
-        if len(nums) < 4:
-            return []
-        nums.sort()
-        for i in range(len(nums)):
-            if nums[i] > target and target >= 0:
-                break
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] > target and target >= 0:
-                    break
-                if j > i + 1 and nums[j] == nums[j-1]:
-                    continue
-                left = j + 1
-                right = len(nums) - 1
-                while left < right:
-                    sum_ = nums[i] + nums[j] +nums[left] + nums[right]
-                    if sum_ > target:
-                        right -= 1
-                    elif sum_ < target:
-                        left += 1
-                    else:
-                        record.append([nums[i],nums[j],nums[left],nums[right]])
-                        while right > left and nums[right] == nums[right - 1]:
-                            right -= 1
-                        while right > left and nums[left] == nums[left + 1]:
-                            left += 1
-                        right -= 1
-                        left += 1
-                    
-        return record
-
-
-
-
+    def removeElement(self, nums: List[int], val: int) -> int:
+        fast = 0 
+        slow = 0
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
         

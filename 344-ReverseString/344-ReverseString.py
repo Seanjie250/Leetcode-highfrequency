77569276a01fa02c5aaf8b4af8f2d266e3e193fd
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-19 10:25:30 +0800

    File updated at 5/19/2025, 10:25:29 AM

--- 344-ReverseString/344-ReverseString.py
+++ 344-ReverseString/344-ReverseString.py
@@ -1,14 +1,7 @@
-# Last updated: 5/19/2025, 10:17:42 AM
+# Last updated: 5/19/2025, 10:25:29 AM
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        head = 0 
-        tail = len(s) - 1
-        while head < tail:
-            s[head],s[tail] = s[tail],s[head]
-            head += 1
-            tail -= 1
-        
+    def reverseWords(self, s: str) -> str:
+        word = s.split()
+        reverser_word = word[::-1]
+        return " ".join(reverser_word)
         

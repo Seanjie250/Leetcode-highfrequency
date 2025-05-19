44a1ed18fadec5493549eb888ce8e19f81bc2290
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-19 15:46:31 +0800

    File updated at 5/19/2025, 3:46:30 PM

--- 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.py
+++ 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.py
@@ -1,16 +1,24 @@
-# Last updated: 5/19/2025, 3:21:28 PM
+# Last updated: 5/19/2025, 3:46:30 PM
+from operator import add, sub, mul
+def div(x:int,y:int) -> int:
+    return int(x/y) if x * y > 0 else -(abs(x)//abs(y))
+
 class Solution:
-    def removeDuplicates(self, s: str) -> str:
+    def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        if len(s) == 1:
-            return s
-        for char in s:
-            if not stack:
-                stack.append(char)
-            elif char != stack[-1]:
-                stack.append(char)
+        self.op_map = {
+            '+' : add,
+            '-' : sub,
+            '*' : mul,
+            '/' : div
+        }
+        for char in tokens:
+            if char not in {'+','-','*','/'}:
+                stack.append(int(char))
             else:
-                stack.pop()
-        return ''.join(stack)
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(self.op_map[char](op1,op2))
+        return stack.pop()
 
         

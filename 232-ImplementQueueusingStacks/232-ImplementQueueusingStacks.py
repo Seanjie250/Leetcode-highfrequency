846c3fd364ee1c2bf4d5aa09e6d1a0091d4acb50
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-19 14:24:21 +0800

    File updated at 5/19/2025, 2:24:20 PM

--- 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.py
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.py
@@ -1,37 +1,44 @@
-# Last updated: 5/19/2025, 2:09:48 PM
-class MyQueue:
+# Last updated: 5/19/2025, 2:24:20 PM
+class MyStack:
 
     def __init__(self):
-        self.stack_in = []
-        self.stack_out = []
+        self.queue_in = deque()
+        self.queue_out = deque()
+        
 
     def push(self, x: int) -> None:
-        self.stack_in.append(x)
+        self.queue_in.append(x)
         
 
     def pop(self) -> int:
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
-        return self.stack_out.pop()
+        if not self.queue_in:
+            return None
+        for i in range(len(self.queue_in) - 1):
+            self.queue_out.append(self.queue_in.popleft())
+        self.queue_in,self.queue_out = self.queue_out,self.queue_in
+        return self.queue_out.popleft()
         
 
-    def peek(self) -> int:
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
-        return self.stack_out[-1]
+    def top(self) -> int:
+        if not self.queue_in:
+            return None
+        for i in range(len(self.queue_in) - 1):
+            self.queue_out.append(self.queue_in.popleft())
+        self.queue_in,self.queue_out = self.queue_out,self.queue_in
+        tmp = self.queue_out.pop()
+        self.queue_in.append(tmp)
+        return tmp
 
         
 
     def empty(self) -> bool:
-        return not self.stack_in and not self.stack_out
+        return len(self.queue_in) == 0
         
 
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
 # obj.push(x)
 # param_2 = obj.pop()
-# param_3 = obj.peek()
+# param_3 = obj.top()
 # param_4 = obj.empty()

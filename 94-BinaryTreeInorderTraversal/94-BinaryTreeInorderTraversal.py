1353ce845a0556f9cd00905bec90b0aed5c1ff3e
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-21 13:12:28 +0800

    File updated at 5/21/2025, 1:12:27 PM

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.py
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 5/21/2025, 9:52:33 AM
+# Last updated: 5/21/2025, 1:12:27 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,19 +6,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-        stack = []
-        if root is None:
-            return []
-        stack.append(root)
-        while stack:
-            node = stack.pop()
-            result.append(node.val)
-            if node.right:
-                stack.append(node.right)
-            if node.left:
-                stack.append(node.left)
-        return result
-        
-        
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        rst = []
+        stack =[]
+        cur = root
+        while stack or cur:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                rst.append(cur.val)
+                cur = cur.right
+        return rst

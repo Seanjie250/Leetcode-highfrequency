0ccdd5a9534d0f19a19846ef515d0ba1b9243530
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-21 09:26:14 +0800

    File updated at 5/21/2025, 9:26:13 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,21 +1,22 @@
-# Last updated: 5/20/2025, 4:30:24 PM
+# Last updated: 5/21/2025, 9:26:13 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        value_index = defaultdict(int)
-        for num in nums:
-            value_index[num] += 1
-        time_index = defaultdict(list)
-        for key in value_index:
-            time_index[value_index[key]].append(key)
-        key = list(time_index.keys())
-        key.sort()
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
-        count = 0
-        while key and count != k:
-            result += time_index[key[-1]]
-            count += 1
-            key.pop()
-        return result[0:k]
- 
+        def DFS(Node):
+            if Node is None:
+                return []
+            
+            result.append(Node.val)
+            DFS(Node.left)
+            DFS(Node.right)
+        DFS(root)
+        return result
+            
         
         

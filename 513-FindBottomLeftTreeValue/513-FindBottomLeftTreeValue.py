401cbc7623732b25c62fcb84c72fe170a2f44697
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-22 15:59:01 +0800

    File updated at 5/22/2025, 3:59:01 PM

--- 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.py
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.py
@@ -1,29 +1,34 @@
-# Last updated: 5/22/2025, 2:26:21 PM
+# Last updated: 5/22/2025, 3:59:01 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        self.result = 0
-        self.max_depth = float('-inf')
-        self.traversal(root,0)
-        return self.result
-
-    def traversal(self,Node,depth):
+    def traversal(self,Node,path,rst) -> List[int]:
+        path.append(Node.val)
         if not Node.left and not Node.right:
-            if depth > self.max_depth:
-                self.max_depth = depth
-                self.result = Node.val
-            return 
+            rst.append(sum(path))
+            return
         if Node.left:
-            self.traversal(Node.left,depth + 1)
+            self.traversal(Node.left,path,rst)
+            path.pop()
         if Node.right:
-            self.traversal(Node.right,depth + 1)
+            self.traversal(Node.right,path,rst)
+            path.pop()
+        return rst
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if root and not root.left and not root.right and root.val == targetSum:
+            return True
+        elif root and not root.left and not root.right and root.val != targetSum:
+            return False
+        path = []
+        rst = []
+        return True if targetSum in self.traversal(root,path,rst) else False
 
-
+        
 
         

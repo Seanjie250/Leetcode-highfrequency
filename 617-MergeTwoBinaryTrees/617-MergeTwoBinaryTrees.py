5d331a311604dbc2533523738706164bbde248db
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-23 16:36:02 +0800

    File updated at 5/23/2025, 4:36:02 PM

--- 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.py
+++ 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.py
@@ -1,4 +1,4 @@
-# Last updated: 5/23/2025, 4:35:39 PM
+# Last updated: 5/23/2025, 4:36:02 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):

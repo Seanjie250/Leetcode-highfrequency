a14a828a687233063f9409fb92accad2ff8e588d
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-24 11:34:08 +0800

    File updated at 5/24/2025, 11:34:07 AM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/24/2025, 11:33:42 AM
+# Last updated: 5/24/2025, 11:34:07 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):

df79e9647f327f75054243ff08586a20ea9a5981
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-24 11:21:25 +0800

    Create file

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -0,0 +1,22 @@
+# Last updated: 5/24/2025, 11:21:23 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validrange(Node,maxm,minm):
+            if not Node:
+                return True
+            if not minm < Node.val < maxm:
+                return False
+            return validrange(Node.left,Node.val,minm) and validrange(Node.right,maxm,Node.val)
+ 
+        maxm = float('inf')
+        minm = float('-inf')
+        return validrange(root,maxm,minm)
+                
+
+        

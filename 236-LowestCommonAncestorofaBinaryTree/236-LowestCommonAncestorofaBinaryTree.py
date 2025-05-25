b811cfa691dccf8b8868db41e20ca38cb0299fc0
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-25 11:37:17 +0800

    Create 236-LowestCommonAncestorofaBinaryTree

---
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.py
@@ -0,0 +1,28 @@
+# Last updated: 5/25/2025, 11:37:15 AM
+class TreeNode:
+    def __int__(self,x):
+        self.val = x
+        self.right = None
+        self.left = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == q or root == p or root is None:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+
+        if left is not None and right is not None:
+            return root
+        if left is not None and right is None:
+            return left
+        elif right is not None and left is None:
+            return right
+        else:
+            return None
+        
+        
+
+        
+        

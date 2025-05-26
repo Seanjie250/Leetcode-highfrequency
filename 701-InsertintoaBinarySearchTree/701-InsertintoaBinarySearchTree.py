86278b40decb16358dd33d03f6f6c8807b290c2d
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-05-26 11:12:16 +0800

    File updated at 5/26/2025, 11:12:16 AM

--- 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.py
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/26/2025, 10:46:46 AM
+# Last updated: 5/26/2025, 11:12:16 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,16 +6,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
-            Node = TreeNode(val)
-            return Node
-
-        if val > root.val:
-            root.right = self.insertIntoBST(root.right,val)
-        if val < root.val:
-            root.left = self.insertIntoBST(root.left,val)
+            return None
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif not root.left and root.right:
+                return root.right
+            elif not root.right and root.left:
+                return root.left
+            else:
+                tmp = root.right
+                while tmp.left:
+                    tmp = tmp.left
+                tmp.left = root.left
+                return root.right
+        if root.val > key:
+            root.left = self.deleteNode(root.left,key)
+        if root.val < key:
+            root.right = self.deleteNode(root.right,key)
         return root
+                
+                
 
+
+
+        
 
         

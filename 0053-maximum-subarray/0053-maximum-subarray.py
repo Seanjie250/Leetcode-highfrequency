9d154f031c93ce51c15e610be955be7a7d743518
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-06-09 10:21:36 +0800

    Time: 19 ms (95.94%), Space: 32.8 MB (39.27%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        rst = float('-inf')
+        count = 0
+        for i in range(len(nums)):
+            count += nums[i]
+            if count > rst:
+                rst = count
+            if count < 0:
+                count = 0
+        return rst
+            
+
+        

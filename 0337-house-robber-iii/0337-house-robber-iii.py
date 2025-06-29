b500f92afbb667505e02da3ff3785d99ff356023
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-06-29 09:58:22 +0800

    Time: 8 ms (22.69%), Space: 19.5 MB (86.59%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def traversal(self,node):
+        if not node:
+            return (0,0)
+        left = self.traversal(node.left)
+        right = self.traversal(node.right)
+
+        val_0 = max(left[0],left[1]) + max(right[0],right[1])
+
+        val_1 = node.val + left[0] + right[0]
+
+        return (val_0,val_1)        
+    def rob(self, root: Optional[TreeNode]) -> int:
+        dp = self.traversal(root)
+        return max(dp)
+
+        

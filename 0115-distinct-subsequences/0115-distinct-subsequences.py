1a82a76562a09214881ee0be30bf71dce9062a5d
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-07-09 11:16:51 +0800

    Time: 415 ms (67.4%), Space: 74.3 MB (41.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -7,6 +7,6 @@ class Solution:
             for j in range(1,len(t) + 1):
                 if s[i - 1] == t[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-                else:
+                else :
                     dp[i][j] = dp[i - 1][j]
         return dp[-1][-1]

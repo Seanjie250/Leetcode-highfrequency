c1aae9a8e686c0f01bd1f6591d23fcb422a294c3
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-10-07 12:57:46 -0700

    Time: 11 ms (72.15%), Space: 17.9 MB (64.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        record = [0] * 26
+        for char in s:
+            record[ord(char) - ord('a')] += 1
+        for char in t:
+            record[ord(char) - ord('a')] -= 1
+        for value in record:
+            while value != 0:
+                return False
+        return True
+            
+
+
+
+

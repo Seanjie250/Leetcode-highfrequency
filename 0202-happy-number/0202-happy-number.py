7597a9dfc6ae2c5e568648a2457f805257bad7a3
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-10-07 13:57:58 -0700

    Time: 0 ms (100%), Space: 17.7 MB (61.52%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def get_sum(self,n:int) -> int:
+        sum = 0
+        while n > 0:
+            sum += (n % 10)**2
+            n = n // 10
+        return sum
+    def isHappy(self, n: int) -> bool:
+        record = set()
+        while n != 1 and n not in record:
+            record.add(n)
+            n = self.get_sum(n)
+        return n == 1
+
+
+        
+        

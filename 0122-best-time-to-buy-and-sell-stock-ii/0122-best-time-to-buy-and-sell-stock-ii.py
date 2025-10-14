189bb221cf05ffb7923eee259ccb3888ae158f76
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-10-14 12:28:10 -0700

    Time: 11 ms (10.01%), Space: 21 MB (9.14%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,8 +4,8 @@ class Solution:
         dp[0][0] = -prices[0]
         dp[0][1] = 0
         for i in range(1,len(prices)):
-            dp[i][0] = max(dp[i - 1][0],dp[i - 1][1] - prices[i])
-            dp[i][1] = max(dp[i - 1][1],dp[i - 1][0] + prices[i])
+            dp[i][0] = max(dp[i - 1][0] , dp[i - 1][1] - prices[i])
+            dp[i][1] = max(dp[i - 1][0] + prices[i] , dp[i - 1][1])
         return dp[len(prices) - 1][1]
             
             

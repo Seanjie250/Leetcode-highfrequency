062f1e14a6a53c4807e6ad96757a0ec236fc54a6
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-10-15 14:27:00 -0700

    Time: 8 ms (94.97%), Space: 19.9 MB (35.86%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -9,7 +9,7 @@ class Solution:
         #right neighber
         for i in range(len(ratings) - 2 ,-1, -1):
             if ratings[i + 1] < ratings[i]:
-                candy[i] = candy[i + 1] + 1
+                candy[i] = max(candy[i + 1] + 1 , candy[i])
 
         return sum(candy)
 

637df5c3d9959e7fc466621236a3099bbc5ac096
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2025-10-25 16:12:49 -0700

    Time: 9435 ms (5.06%), Space: 20.3 MB (98.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,23 @@
+from collections import Counter
 class Solution:
+    def check_anagrams(self,s,t):
+        if len(s) != len(t):
+            return False
+        key_s = ''.join(sorted(s))
+        key_t = ''.join(sorted(t))
+        return True if key_s == key_t else False
+        
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
-        for s in strs:
-            key = ''.join(sorted(s))
-            groups[key].append(s)
-        return list(groups.values())
+        if len(strs) == 1 or len(strs) == 0:
+            return [strs]
+        ans = []
+        while strs:
+            rst = [strs[0]]
+            for i in range(1,len(strs)):
+                if self.check_anagrams(strs[0],strs[i]):
+                    rst.append(strs[i])
+            ans.append(rst)
+            strs = [s for s in strs if s not in rst]
+        return ans
+
+        

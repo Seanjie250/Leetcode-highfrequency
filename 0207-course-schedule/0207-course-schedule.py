dcce392139d12e94e674be87f47831b31e153116
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-01 14:55:33 -0800

    Time: 4 ms (65.08%), Space: 20.4 MB (46.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        n = numCourses
-        adj = [[] for _ in range(n)]
-        indegree = [0] * n
-        ans = []
+        graph = [[] for _ in range(numCourses)]
+        degree = [0] * numCourses
+        for course , pre in prerequisites:
+            graph[pre].append(course)
+            degree[course] += 1
+        
+        q = deque([i for i in range(numCourses) if degree[i] == 0])
 
-        for pair in prerequisites:
-            course = pair[0]
-            prerequisites = pair[1]
-            adj[prerequisites].append(course)
-            indegree[course] += 1
+        count = 0
 
-        queue = deque()
-        for i in range(n):
-            if indegree[i] == 0:
-                queue.append(i)
-        while queue:
-            current = queue.popleft()
-            ans.append(current)
-            for next_course in adj[current]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    queue.append(next_course)
-
-        return len(ans) == n
+        while q:
+            cur = q.popleft()
+            count += 1
+            for course in graph[cur]:
+                degree[course] -= 1
+                if degree[course] == 0:
+                    q.append(course)
+        return count == numCourses
                 
 
-            
 
 
 

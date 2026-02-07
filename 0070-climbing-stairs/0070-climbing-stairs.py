f98d0749f5b3b054ebcbc2f683028f2e0f1ed04b
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-07 14:21:04 -0800

    Time: 0 ms (100%), Space: 19.4 MB (10.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -6,7 +6,8 @@ class Solution:
         elif n == 2:
             return 2
         dp[0] = 0
-        dp[1] = 1    
-        for i in range(2 , n + 1):
-            dp[i] = max(dp[i - 1] + 1 , dp[i - 2] + 1)
+        dp[1] = 1  
+        dp[2] = 2  
+        for i in range(3 , n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
         return dp[-1]

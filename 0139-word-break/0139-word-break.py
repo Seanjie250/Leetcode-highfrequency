4f115be4dbbefd6aaf57164dd3b1b822684c3b79
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-07 15:04:02 -0800

    Time: 3 ms (69.92%), Space: 19.3 MB (47.35%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,17 @@
 class Solution:
-    def backtracking(self,s,wordDict,startindex,memo):
-        if startindex >= len(s):
-            return True
-        if startindex in memo:
-            return False
-        for i in range(startindex,len(s)):
-            word = s[startindex:i + 1]
-            if word in wordDict and self.backtracking(s,wordDict,i + 1,memo):
-                return True
-        memo.add(startindex)
-        return False
-
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wordDict = set(wordDict)
-        memo = set()
-        return self.backtracking(s,wordDict,0,memo)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for word in wordDict:
+            if s.startswith(word):
+                dp[len(word)] = True
+        for i in range(1 , len(s) + 1):
+            for word in wordDict:
+                j = len(word)
+                if s[i - j:].startswith(word):
+                    dp[i] = dp[i - j] or dp[i]
+        return dp[len(s)]
+
+
+
+        

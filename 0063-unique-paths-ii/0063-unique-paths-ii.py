d0f3608b5a5b205f5d2d22733535d942fc2de224
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-08 11:42:13 -0800

    Time: 0 ms (100%), Space: 19.5 MB (19.78%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,19 +1,27 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        dp = [[0] * n for _ in range(m)]
-        if obstacleGrid[0][0] == 1:
-            return 0
-        dp[0][0] = 1 
-        for i in range(1,m):
-            if obstacleGrid[i][0] == 0 and dp[i - 1][0] == 1:
-                    dp[i][0] = 1
-        for i in range(1,n):
-            if obstacleGrid[0][i] == 0 and dp[0][i - 1] == 1:
-                    dp[0][i] = 1
-        for i in range(1,m):
-            for j in range(1,n):
+        
+        m , n = len(obstacleGrid) , len(obstacleGrid[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        i , j = 0 , 0
+        while i < m:
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = 1
+                i += 1
+            else:
+                break
+        while j < n:
+            if obstacleGrid[0][j] == 0:
+                dp[0][j] = 1
+                j += 1
+            else:
+                break
+        for i in range(1 , m):
+            for j in range(1 , n):
                 if obstacleGrid[i][j] == 0:
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-        return dp[m-1][n-1]
+                else:
+                    dp[i][j] = 0
+        
+        return dp[m - 1][n - 1]
+        

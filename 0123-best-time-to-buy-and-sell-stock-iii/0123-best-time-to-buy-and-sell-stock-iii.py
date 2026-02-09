3a35c2b2c96a3be9dd1dcd2c962c879236bd6328
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-09 14:21:38 -0800

    Time: 691 ms (25.76%), Space: 67.1 MB (18.63%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,22 +1,14 @@
 class Solution:
-
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        left_profit = [0] * len(prices)
-        right_profit = [0] * len(prices)
+        n = len(prices) # days
+        dp = [[[0] * 2  for _ in range(3)] for _ in range(n)]
+        for i in range(1, 3):
+            dp[0][i][1] = -prices[0]
+        
+        for i in range(1, n ):
+            for j in range(1, 3):
+                dp[i][j][0] = max(prices[i] + dp[i - 1][j][1] , dp[i - 1][j][0])
+                dp[i][j][1] = max(dp[i - 1][j][1] , dp[i - 1][j - 1][0] - prices[i])
+        return dp[-1][-1][0]
 
-        min_price = prices[0]
-        for i in range(1,len(prices)):
-            min_price = min(prices[i],min_price)
-            left_profit[i] = max(left_profit[i - 1],prices[i] - min_price)
 
-        max_price = prices[-1]
-        for i in range(len(prices) - 2,-1,-1):
-            max_price = max(prices[i] , max_price)
-            right_profit[i] = max(right_profit[i + 1],max_price - prices[i])
-
-        max_profit = float('-inf')
-        for i in range(len(prices)):
-            max_profit = max(max_profit,right_profit[i] + left_profit[i])
-        return max_profit

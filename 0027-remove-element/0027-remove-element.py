670516a6b16c06026ee103555632aa687870a1d3
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-16 15:38:28 -0800

    Time: 0 ms (100%), Space: 19.4 MB (43.39%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,19 +1,12 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        fast , slow = 0 , 0
+        slow , fast = 0 , 0
+        count = 0 
         while fast < len(nums):
             if nums[fast] != val:
                 nums[slow] = nums[fast]
                 slow += 1
+                count += 1
             fast += 1
-        return slow
-
-            
-
-
-
-
-            
-
-
+        return count
         

c9b198eaf47a309f120b0138197540f91216036f
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-16 15:57:45 -0800

    Time: 131 ms (28.7%), Space: 19.9 MB (9.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,31 +1,31 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_count = Counter(t)
-        l = 0
-        rst = [-1, -1]
-        have = 0
-        need = len(t_count)
-        minlength = float('inf')
-        window_count = Counter()
-        for r in range(len(s)):
-            c = s[r]
-            window_count[c] = window_count.get(c,0) + 1
-            if c in t_count and t_count[c] == window_count[c]:
-                have += 1
-            while have == need:
-                if (r - l + 1) < minlength:
-                    minlength = r - l + 1
-                    rst = [l , r]
-                window_count[s[l]] -= 1
-                if s[l] in t_count and t_count[s[l]] > window_count[s[l]]:
-                    have -= 1
-                l += 1
-        l , r = rst
-        return s[l : r + 1] if minlength != float('inf') else ""
-
-
-
+        need = Counter(t)
+        window = Counter()
+        left , right = 0 , 0
+        valid = 0
+        length = float('inf')
+        while right < len(s):
+            ch =  s[right]
+            right += 1
+            if ch in need:
+                window[ch] += 1
+                if window[ch] == need[ch]:
+                    valid += 1
             
-
+            while valid == len(need):
+                if right - left < length:
+                    start = left
+                    length = right - left
+                print(left)
+                d = s[left]
+                left += 1
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -=1
+        return "" if length == float('inf') else s[start : start + length]
+            
+                
+                
         

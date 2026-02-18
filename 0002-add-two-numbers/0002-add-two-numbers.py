3fcf2c7451672ddb8b17eb1e01f06fe1cf3ccb25
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-18 11:18:31 -0800

    Time: 15 ms (7.89%), Space: 19.6 MB (9.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,38 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse(self,head):
-        prev = None
-        cur = head
-        while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
-        return prev
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        number_1 , number_2 = 0 , 0
-        dummy_1 = ListNode(0)
-        dummy_2 = ListNode(0)
-        dummy_1.next = self.reverse(l1)
-        dummy_2.next = self.reverse(l2)
-        node_1 = dummy_1.next
-        node_2 = dummy_2.next
-        while node_1:
-            number_1 = number_1 * 10 + node_1.val
-            node_1 = node_1.next
-        while node_2:
-            number_2 = number_2 * 10 + node_2.val
-            node_2 = node_2.next
-        number_3 = number_1 + number_2
-        answer = ListNode(0)
-        current = answer
-        if number_3 == 0:
-            return ListNode(0)
-        while number_3 > 0:
-            digit = number_3 % 10
-            current.next = ListNode(digit)
-            number_3 = number_3 // 10
-            current = current.next
-        return answer.next
+        dummy = ListNode(-1)
+        p = dummy
+        carry = 0
+        p1, p2 = l1 , l2
+        while p1 is not None or p2 is not None or carry > 0:
+            val = carry
+            if p1 is not None:
+                val += p1.val
+                p1 = p1.next
+            if p2 is not None:
+                val += p2.val
+                p2 = p2.next
+            carry = val // 10
+            val = val % 10
+            print(val)
+            p.next = ListNode(val)
+            p = p.next
+           
+            print(carry)
+        return dummy.next
+        

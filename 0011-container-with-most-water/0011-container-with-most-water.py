54da2a0320da58aab8074756d29a0f73e2a8699e
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-18 19:48:31 -0800

    Time: 57 ms (69.01%), Space: 29.7 MB (48.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,20 +1,14 @@
 class Solution:
-    def calculate_area(self, height, i, j):
-        return (j - i) * min(height[i], height[j])
-
     def maxArea(self, height: List[int]) -> int:
-        i, j = 0, len(height) - 1
-        max_area = 0
-
-        while i < j:
-            # calculate current area
-            area = self.calculate_area(height, i, j)
-            max_area = max(max_area, area)
-
-            # move the smaller line inward
-            if height[i] < height[j]:
-                i += 1
+        maxm = float('-inf')
+        left , right = 0 , len(height) - 1
+        while left < right:
+            width = right - left
+            maxm = max(maxm , width * min(height[left] , height[right]))
+            if height[left] < height[right]:
+                left += 1
             else:
-                j -= 1
+                right -= 1
+        return maxm
 
-        return max_area
+        

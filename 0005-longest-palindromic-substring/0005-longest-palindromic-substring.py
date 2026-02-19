22c2fa37d809d9e2d8536eafb3b28b3e0b091c47
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-18 20:22:02 -0800

    Time: 227 ms (84.99%), Space: 19.3 MB (73.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,32 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ans = 1
-        l = len(s)
+        if not s:
+            return ''
+        def expandfromcenter(s , left , right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                right += 1
+                left -= 1
+            return right - left - 1
+        
         start = 0
-        dp = [[False for _ in range(l)] for _ in range(l)]
-        for i in range(l):
-            dp[i][i] = True
-        
-        for length in range(2 , l + 1):
-            for i in range(0 , l - length + 1):
-                j = i + length - 1
+        end = 0
 
-                if s[i] != s[j]:
-                    dp[i][j] = False
-                else:
-                    if length == 2:
-                        dp[i][j] = True
-                    else:
-                        dp[i][j] = dp[i + 1][j - 1]
-                if dp[i][j] and length > ans:
-                    ans = max(ans , length)
-                    start = i
+        for i in range(len(s)):
+            odd = expandfromcenter(s, i , i)
+            even = expandfromcenter(s, i , i + 1)
+            maxm = max(odd ,even)
 
-        return s[start : start + ans ]
-            
-                    
-                
+            if maxm > end - start:
+                start = i - (maxm - 1) // 2
+                end = i + maxm // 2
 
-                
-
-        
+        return s[start : end + 1]        

40222d30a680abe213ef293c605e3655d9b28aae
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-20 13:47:13 -0800

    Time: 11 ms (8.3%), Space: 20.4 MB (60.09%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,26 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        degree = [0] * numCourses
+        degrees = [0] * numCourses
         graph = defaultdict(list)
-        q = deque()
-        rst = []
         for course , prerequisite in prerequisites:
             graph[prerequisite].append(course)
-            degree[course] += 1
-        
-        for i in range(numCourses):
-            if degree[i] == 0:
-                q.append(i)
-        
-        while q:
-            cour = q.popleft()
-            print(cour)
-            for c in graph[cour]:
-                degree[c] -= 1
-                if degree[c] == 0:
-                    q.append(c)
-            
-            rst.append(cour)
-        return rst if len(rst) == numCourses else []
+            degrees[course] += 1
+        print(graph)
 
-        
+        q = deque()
+
+        for i in range(len(degrees)):
+            if degrees[i] == 0:
+                q.append(i)
+        rst = []
+        while q:
+            for i in range(len(q)):
+                cur = q.popleft()
+                
+                for courses in graph[cur]:
+                    degrees[courses] -= 1
+                    if degrees[courses] == 0:
+                        q.append(courses)
+                rst.append(cur)
+        return rst if len(rst) == numCourses else []

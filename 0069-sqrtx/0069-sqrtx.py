cff93d652da4414c90052f6b4b3a502f99f8a699
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-20 11:03:01 -0800

    Time: 2431 ms (5.01%), Space: 19.3 MB (67.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,11 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x < 2:
-            return x
-        root = x // 2
-
-        while root * root > x :
-            root = (root + x // root) // 2
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        for i in range(x):
+            if i * i <= x and (i + 1) * (i + 1) > x:
+                return i
         
-        return root
         

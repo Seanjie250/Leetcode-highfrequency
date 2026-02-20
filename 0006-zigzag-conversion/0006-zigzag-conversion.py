f988d725ea1eb2f903f9c113a335fe8c3dcc6742
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-20 12:36:59 -0800

    Time: 95 ms (7.82%), Space: 26.7 MB (5.01%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,42 +2,21 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        store = [[''] * (len(s)) for _ in range(numRows)]
-        s = deque(s)
-        i = 0
-        row = 0
+        n = len(s)
+        grid = [[''] * n for _ in range(numRows)]
+
         down = True
-        while s:
-            store[row][i] = s.popleft()
-            if down:
-                row += 1
+        j = 0
+        for i in range(len(s)):
+            ch = s[i]
+            grid[j][i] = ch
+            if down == True:
+                j += 1
             else:
-                row -= 1 
-            if row == 0:
-                down = True
-            elif row == numRows - 1:
+                j -= 1
+            if j == numRows - 1:
                 down = False
-            i += 1
-
-        rst = ''
-        for r in store:
-            rst += "".join(r)
-
-        return rst
-
-        
-
-
-            
-
-        
-
-
-            
-            
-    
-        
-           
-
-
+            elif j == 0:
+                down = True
+        return ''.join(''.join(row) for row in grid)
         

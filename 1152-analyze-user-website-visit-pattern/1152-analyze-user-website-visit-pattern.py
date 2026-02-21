bbba2544681784d50b14edfb4a5854add5c14ab7
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-21 15:46:46 -0800

    Time: 7 ms (77.41%), Space: 20.2 MB (29.36%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,38 +1,26 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        logs = sorted(zip(timestamp , username , website))
-        
-        user_web = defaultdict(list)
-        for t , u , w in logs:
-            user_web[u].append(w)
-        
-        tuple_count = Counter()
 
-        for u , w in user_web.items():
+        logs = sorted(zip(timestamp , username , website))
+
+        pattern = defaultdict(list)
+
+        for t , u , w in logs:
+            pattern[u].append(w)
+        tuple_count = Counter()
+        for u,w in pattern.items():
             if len(w) < 3:
                 continue
+            tuple_type = set(combinations(w , 3))
+            for tuple_ in tuple_type:
+                tuple_count[tuple_] += 1
             
-            pattern_type = set(combinations(w , 3))
-
-            for tuples in pattern_type:
-                tuple_count[tuples] += 1
-
-
         best_score = -1
         best_pattern = None
         for tuple_ , count in tuple_count.items():
-            if count > best_score or (count == best_score and tuple_ < best_pattern):
-                best_score = count
+            if count > best_score or (best_score == count and tuple_ < best_pattern):
+                best_score = count 
                 best_pattern = tuple_
-                print(best_score)
-                print(best_pattern)
-        
         return best_pattern
             
-        
-                
 
-
-
-
-        

bb166ed1bd1c8f23ef1f3c718e4830c7d55acbf2
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-21 11:07:28 -0800

    Time: 2919 ms (90.25%), Space: 19.6 MB (46.54%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,37 +1,39 @@
 class Solution:
+    directions = [[0,1] , [0 , -1] , [1, 0] , [-1 , 0]]
+    def dfs(self, board , visited , i ,j ,word ,count):
+        if board[i][j] != word[count]:
+            return False
+        if count == len(word) - 1:
+            return True
+        visited[i][j] = True
+        m , n = len(board) , len(board[0])
+        for dx , dy in self.directions:
+            new_x = i + dx
+            new_y = j + dy
+            if new_x < m and new_x >= 0 and new_y >= 0 and new_y < n and not visited[new_x][new_y] and board[new_x][new_y] == word[count + 1]:
+                if self.dfs(board , visited , new_x ,new_y ,word ,count + 1):
+                    return True
+        visited[i][j] = False     
+        return False
+                
+        
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        path = []
-
+        
+        length = len(word)
+        m , n = len(board) , len(board[0])
+        ans = False
         visited = [[False] * n for _ in range(m)]
-
-        def dfs(i,j,start):
-            if start == len(word):
-                return True
-            if i < 0 or i >= m or j < 0 or j >= n:
-                return False
-            if visited[i][j]:
-                return False
-            if board[i][j] != word[start]:
-                return False
-            visited[i][j] = True
-            found = (
-                dfs(i + 1,j,start + 1) or 
-                dfs(i - 1,j,start + 1) or 
-                dfs(i,j - 1,start + 1) or 
-                dfs(i,j + 1,start + 1)
-            )
-            visited[i][j] = False
-            return found
         for i in range(m):
             for j in range(n):
-                if dfs(i,j,0):
-                    return True
+                if board[i][j] == word[0]:
+                    if self.dfs(board , visited , i ,j ,word,0):
+                        return True
         return False
+                   
 
                 
-
+                    
+        
 
 
         

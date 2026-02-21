848a92b5bd41f2fd3d07ef0f181286e67ac418d8
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-21 11:35:03 -0800

    Time: 0 ms (100%), Space: 19.4 MB (23.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,23 @@
-from collections import deque
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         for ch in s:
             if ch == '(':
                 stack.append(')')
-            elif ch == '{':
-                stack.append('}')
-            elif ch == '[':
+            if ch == '[':
                 stack.append(']')
-            elif not stack or stack[- 1] != ch:
-                return False
-            elif stack[-1] == ch:
-                stack.pop()
+            if ch == '{':
+                stack.append('}')
+            if ch == ')':
+                if not stack or stack.pop() != ch:
+                    return False
+            if ch == ']':
+                if not stack or stack.pop() != ch:
+                    return False
+            if ch == '}':
+                if not stack or stack.pop() != ch:
+                    return False
         return True if not stack else False
-                
+                   
+
+            

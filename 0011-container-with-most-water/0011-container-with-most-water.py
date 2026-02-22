a136eb86d20f6621b13d07b0f276e3e9d399977d
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-22 14:21:34 -0800

    Time: 59 ms (60.06%), Space: 29.8 MB (12.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,12 +3,10 @@ class Solution:
         maxm = float('-inf')
         left , right = 0 , len(height) - 1
         while left < right:
-            width = right - left
-            maxm = max(maxm , width * min(height[left] , height[right]))
-            if height[left] < height[right]:
+            maxm = max(maxm , (min(height[left] , height[right]) * (right - left)))
+            if height[left] <= height[right]:
                 left += 1
             else:
                 right -= 1
         return maxm
-
         

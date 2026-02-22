6566674b0ede8ce8cb9a8e73e89c1ea43379b24a
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-21 20:18:41 -0800

    Time: 86 ms (6.33%), Space: 22.4 MB (17.36%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -2,10 +2,10 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         record = {0 : 1}
         prefix = 0
-        rst = 0
+        ans = 0
         for num in nums:
             prefix += num
-            rst += record.get(prefix - k , 0)
+            ans += record.get(prefix - k , 0)
+            print(ans)
             record[prefix] = record.get(prefix , 0) + 1
-        return rst 
-        
+        return ans

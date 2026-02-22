16401749ac0852e874f701a5e6e66e33c79acf46
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-22 11:48:00 -0800

    Time: 243 ms (61.28%), Space: 21.8 MB (47.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,20 @@
 class Solution:
-    directions = [[1, 0] , [0, 1] , [-1 , 0] , [0, -1]]
-    def dfs(self, grid, visited , i , j):
+    directions = [[1, 0] , [-1 , 0] , [0 , 1], [0 , -1]]
+    def dfs(self , grid , visited , i , j):
+        m , n = len(grid) , len(grid[0])
         visited[i][j] = True
         for dx , dy in self.directions:
-            new_x = dx + i
-            new_y = dy + j
-            if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and  not visited[new_x][new_y] and grid[new_x][new_y] == '1':
-                self.dfs(grid , visited , new_x , new_y)
+            new_x , new_y = dx + i , dy + j
+            if new_x >= 0 and new_x < m and new_y >= 0 and new_y < n and not visited[new_x][new_y] and grid[new_x][new_y] == '1':
+                self.dfs(grid, visited , new_x , new_y)
     def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
         m , n = len(grid) , len(grid[0])
         visited = [[False] * n for _ in range(m)]
-        count = 0
         for i in range(m):
             for j in range(n):
                 if not visited[i][j] and grid[i][j] == '1':
+                    
                     self.dfs(grid , visited , i , j)
                     count += 1
         return count

51527168eb75bb8a17c11eadf76e30027c62b02e
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-02-22 15:00:59 -0800

    Time: 7 ms (71.53%), Space: 22.4 MB (79.17%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,16 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals)
+        intervals.sort(key = lambda x : x[0])
         rst = []
+        if not intervals:
+            return []
         rst.append(intervals[0])
-        for i in range(1,len(intervals)):
-            if rst[-1][1] >= intervals[i][0]:
+
+        for i in range(1 , len(intervals)):
+            if rst[-1][1] < intervals[i][0]:
+                rst.append(intervals[i])
+            else:
                 rst[-1][0] = min(rst[-1][0] , intervals[i][0])
                 rst[-1][1] = max(rst[-1][1] , intervals[i][1])
-            else:
-                rst.append(intervals[i])
-        print(rst)
+
         return rst
-
-
         

ba140f36edaf703e751dd3f65b0eeb27b39d39e6
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-05 15:33:01 -0800

    Time: 3 ms (91.71%), Space: 19.3 MB (87.56%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.py
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.py
@@ -1,36 +1,53 @@
 class Solution:
-    def calculate_(self, s , start , end , rightpair) -> int:
-        stack = []
-        num = 0
-        i = start
-        pre_op = '+'
-        while i <= end:
-            if s[i].isdigit():
-                num = num * 10 + int(s[i])
-            elif s[i] == '(':
-                num = self.calculate_(s , i + 1 , rightpair[i] - 1 , rightpair)
-                i = rightpair[i]
-            if s[i] in '+-/*' or i == end:
-                if pre_op == '+':
+    def calculate(self, s: str) -> int:
+        i = 0
+        def helper():
+            n = len(s)
+            nonlocal i
+            stack = []
+            sign = '+'
+            num = 0
+            while i < n:
+                ch = s[i]
+                if ch == ' ':
+                    i += 1
+                    continue
+                if ch.isdigit():
+                    num = num * 10 + int(ch)
+                    i += 1
+                    continue
+                if ch == '(':
+                    i += 1
+                    num = helper()
+                    continue
+                if sign == '+':
                     stack.append(num)
-                elif pre_op == '-':
+                elif sign == '-':
                     stack.append(-num)
-                elif pre_op == '/':
-                    stack.append(math.trunc(stack.pop() / num))
-                elif pre_op == '*':
-                    stack.append(stack.pop() * num)
+                elif sign == '*':
+                    stack[-1] = stack[-1] * num
+                elif sign == '/':
+                    stack[-1] = int(stack[-1] / num)
+                if ch == ')':
+                    i += 1
+                    return sum(stack)
                 num = 0
-                pre_op = s[i]
-            i += 1
-        return sum(stack)
-    def calculate(self, s:str) -> int:
-        rightpair = {}
-        stack = []
-        for i,ch in enumerate(s):
-            if ch == '(':
-                stack.append(i)
-            elif ch == ')':
-                rightpair[stack.pop()] = i
-        
-        return self.calculate_(s, 0 , len(s) - 1 , rightpair)
+                sign = ch 
+                i += 1
 
+            if sign == '+':
+                stack.append(num)
+            elif sign == '-':
+                stack.append(-num)
+            elif sign == '*':
+                stack[-1] = stack[-1] * num
+            elif sign == '/':
+                stack[-1] = int(stack[-1] / num)
+            return sum(stack)
+        return helper()
+                
+
+            
+        
+
+        

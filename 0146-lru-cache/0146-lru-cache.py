84868251d62a355bd13d4b72d7075ee082ddac89
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-05 12:12:11 -0800

    Time: 100 ms (85.74%), Space: 78.7 MB (26.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,53 +1,52 @@
 class ListNode:
-    def __init__(self , key, val):
-        self.val = val
+    def __init__(self , key , val):
         self.key = key
+        self.val = val
         self.next = None
         self.prev = None
 class LRUCache:
     def __init__(self, capacity: int):
         self.head = ListNode(0 , 0)
         self.tail = ListNode(0 , 0)
-        self.capacity = capacity
-        self.cache = {}
         self.head.next = self.tail
         self.tail.prev = self.head
-    def add_to_head(self , node):
-        nxt = self.head.next
+        self.cache = {}
+        self.capacity = capacity
+    def remove_node(self , node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+    def add_to_head(self,node):
+        next_node = self.head.next
+        node.next = next_node
+        next_node.prev = node
         self.head.next = node
         node.prev = self.head
-        node.next = nxt
-        nxt.prev = node
-    def remove(self , node):
-        prev = node.prev
-        nxt = node.next
-        prev.next= nxt
-        nxt.prev = prev
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if not key in self.cache:
             return -1
-        else:
-            node = self.cache[key]
-            val = node.val
-            self.remove(node)
-            self.add_to_head(node)
-            return val
+        node = self.cache[key]
+        val = node.val
+        self.remove_node(node)
+        self.add_to_head(node)
+        return val
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             node.val = value
-            self.remove(node)
+            self.remove_node(node)
             self.add_to_head(node)
         else:
-            if len(self.cache) == self.capacity:
-                delete = self.tail.prev 
-                key_ = delete.key
-                self.remove(delete)
-                del self.cache[key_]
-            node = ListNode(key ,value)
+            node = ListNode(key , value)
+            if len(self.cache) >= self.capacity:
+                tail_node = self.tail.prev
+                self.remove_node(tail_node)
+                del self.cache[tail_node.key]
+            self.cache[key] = node
             self.add_to_head(node)
-            self.cache[key] = (node)
 
+        
 
 
 # Your LRUCache object will be instantiated and called as such:

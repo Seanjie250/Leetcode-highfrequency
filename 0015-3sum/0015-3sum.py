fc485825f824212aaa620eb6292e44c425957596
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-05 09:21:37 -0800

    Time: 559 ms (76.07%), Space: 21.9 MB (94.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,31 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        n = len(nums)
+    def twoSum(self, nums , target):
         rst = []
-        for i in range(n):
+        left , right = 0 , len(nums) - 1
+        while left < right:
+            if nums[left] + nums[right] > target:
+                right -= 1
+            elif nums[left] + nums[right] < target:
+                left += 1
+            else:
+                rst.append((nums[left] , nums[right]))
+                while left < right and  nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and  nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+        return rst
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums = sorted(nums)
+        rst = []
+        for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            left , right = i + 1 , n - 1
-            while left < right:
-
-                if nums[i] + nums[left] + nums[right] == 0:
-                    rst.append([nums[i] , nums[left] , nums[right]])
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
-                elif nums[i] + nums[left] + nums[right] > 0:
-                    right -= 1
-                else:
-                    left += 1
+            target = -nums[i]
+            for a , b  in self.twoSum(nums[i + 1:] , target):
+                rst.append([nums[i] , a , b ])
+    
         return rst
 
-                
-                
-
-
-            
         

2ea64b98deb7dd2468fa3ea7845d846baa97fc24
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-05 10:13:47 -0800

    Time: 0 ms (100%), Space: 19.3 MB (78.28%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -0,0 +1,12 @@
+from math import gcd
+class Solution:
+    def canMeasureWater(self, x: int, y: int, target: int) -> bool:
+        if x + y < target:
+            return False
+        if target == 0:
+            return True
+        return target % gcd(x , y) == 0
+        
+        
+
+

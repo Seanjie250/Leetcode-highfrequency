f83ee96d6d8fe9d1977fbc71571c88defeb58f6b
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-07 16:44:26 -0800

    Time: 0 ms (100%), Space: 19.4 MB (59.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -9,7 +9,7 @@ class Solution:
             if nums[white] == 0:
                 nums[red] , nums[white] = nums[white] ,nums[red]
                 red += 1
-                
+                white += 1
             elif nums[white] == 2:
                 nums[blue] , nums[white] = nums[white] ,nums[blue]
                 blue -= 1

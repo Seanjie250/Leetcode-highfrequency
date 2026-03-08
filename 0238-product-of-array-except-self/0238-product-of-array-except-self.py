d07f093822abe7b404de908777fc7e67f4269273
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-08 15:34:41 -0700

    Time: 44 ms (10.21%), Space: 27.7 MB (19.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,17 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        rst = [1] * n
-        prefix = 1
-        for i in range(n):
-            rst[i] = prefix
-            prefix *= nums[i]
+        left_time = [0] * len(nums)
+        right_time = [0] * len(nums)
+        rst = [0] * len(nums)
+        left_time[0] = nums[0]
+        right_time[- 1] = nums[-1]
+        for i in range(1, len(nums)):
+            left_time[i] = nums[i] * left_time[i - 1]
+        for i in range(len(nums) - 2 , - 1, - 1):
+            right_time[i] = nums[i] * right_time[i + 1]
         
-        subfix = 1
-        for i in range(n -1 , - 1, -1):
-            rst[i] *= subfix
-            subfix *= nums[i]
+        print(left_time)
+        print(right_time)
+        rst[0] = right_time[1]
+        rst[-1] = left_time[-2]
+
+        for i in range(1 , len(nums) - 1):
+            rst[i] = left_time[i - 1] * right_time[i + 1]
         return rst
+
+
+        
+
 
             
 
+        
+        

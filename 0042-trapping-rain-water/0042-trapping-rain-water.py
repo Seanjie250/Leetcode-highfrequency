8c444b1b18b2f600a36673a873a9de433c3780fe
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-08 17:00:55 -0700

    Time: 11 ms (46.58%), Space: 21.4 MB (7.81%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,13 +3,11 @@ class Solution:
         stack = []
         rst = 0
         for i in range(len(height)):
-            while stack and height[i] > height[stack[-1]]:
+            while stack and height[stack[-1]] < height[i]:
                 mid = stack.pop()
                 if stack:
-                    h = min(height[i] , height[stack[-1]]) - height[mid]
-                    rst += h * (i - stack[-1] - 1)
-            
+                    h = min(height[stack[-1]] , height[i])
+                    rst += (h - height[mid]) * (i - stack[-1] - 1)
             stack.append(i)
         return rst
-            
         

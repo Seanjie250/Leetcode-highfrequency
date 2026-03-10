abf32b4b98f3e5a5abe892f73cf824ee8b7d37ac
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-10 13:21:24 -0700

    Time: 3002 ms (89.94%), Space: 19.4 MB (93.66%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,31 @@
 class Solution:
-    directions = [[0,1] , [0 , -1] , [1, 0] , [-1 , 0]]
-    def dfs(self , board , visited , word , i , j):
-        
+    directions = [[1, 0] , [-1 , 0] , [0 , -1], [0 , 1]]
+    def dfs(self , board , visited , i , j , word):
+        m , n = len(board) , len(board[0])
         if not word:
             return True
-        m ,n = len(board) , len(board[0])
         for dx , dy in self.directions:
-            new_x , new_y = dx + i , dy + j
-            if new_x >= 0 and new_x < m and new_y >= 0 and new_y < n and not visited[new_x][new_y] and board[new_x][new_y] == word[0]:
+            new_x , new_y = i + dx , j + dy
+            if 0 <= new_x < m and 0 <= new_y < n and not visited[new_x][new_y] and board[new_x][new_y] == word[0]:
                 visited[new_x][new_y] = True
-                if self.dfs(board , visited , word[1:] , new_x , new_y):
+                if self.dfs(board , visited , new_x , new_y , word[1:]):
                     return True
                 visited[new_x][new_y] = False
         return False
+                   
 
-
+        
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m ,n = len(board) , len(board[0])
+        m , n = len(board) , len(board[0])
         visited = [[False] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                if board[i][j] == word[0]:
+                if board[i][j] == word[0] and not visited[i][j]:
                     visited[i][j] = True
-                    if self.dfs(board , visited , word[1:], i , j):
+                    if self.dfs(board, visited, i , j ,word[1:]):
                         return True
                     visited[i][j] = False
         return False
-                    
 
+        
         

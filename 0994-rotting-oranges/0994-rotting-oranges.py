240638b23769a68b96754e6ca9b863c10fbef23c
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-10 11:42:10 -0700

    Time: 4 ms (36.25%), Space: 19.3 MB (74.43%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,41 +1,40 @@
 class Solution:
-    directions = [[1,0] , [-1 , 0] , [0, 1] , [0 , -1]]
+    
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        directions = [[1,0] , [-1 , 0], [0, - 1], [0, 1]]
+        count = 0
+        q = deque()
+       
         m , n = len(grid) , len(grid[0])
         visited = [[False] * n for _ in range(m)]
-        q = deque()
-        number = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     q.append((i , j))
                     visited[i][j] = True
                 if grid[i][j] == 1:
-                    number += 1
-        if number == 0:
-            return 0
+                    count += 1
+        rotten = 0
         times = 0
-        count = 0
+        while not q and count == 0:
+            return 0
+        while not q and count != 0:
+            return -1
         while q:
-            rottened = 0
-            for i in range(len(q)):
-                x, y = q.popleft()
-                for dx , dy in self.directions:
-                    new_x = dx + x
-                    new_y = dy + y
-                    if new_x < m and new_x >= 0 and new_y >= 0 and new_y < n and not visited[new_x][new_y] and grid[new_x][new_y] == 1:
-                        count += 1
-                        rottened += 1
+            times += 1
+            for _ in range(len(q)):
+                i , j = q.popleft()
+                for dx , dy in directions:
+                    new_x = i + dx 
+                    new_y = j + dy
+                    if 0<= new_x < m and 0<= new_y < n and grid[new_x][new_y] == 1 and not visited[new_x][new_y] :
                         visited[new_x][new_y] = True
-                        grid[new_x][new_y] = 2
+                        grid[new_x][new_y] == 2
+                        rotten += 1
                         q.append((new_x , new_y))
-            if rottened > 0:
-                times += 1 
+            
 
-        return times if count == number else -1
-                    
-                        
-                    
+        return times - 1 if rotten == count else -1
 
 
         

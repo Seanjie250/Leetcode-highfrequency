5579d494ebc2d2fd7351307f2f1d4d93d0dfac2c
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-11 13:09:16 -0700

    Time: 3586 ms (5.68%), Space: 333.9 MB (5.05%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+
+        def findPalindromes(s, rst):
+
+            for i in range(len(s)):
+                left, right = i, i
+                while left >= 0 and right < len(s) and s[left] == s[right]:
+                    if right - left + 1 >= k:
+                        rst.append([left, right])
+                    left -= 1
+                    right += 1
+
+            for i in range(len(s)):
+                left, right = i, i + 1
+                while left >= 0 and right < len(s) and s[left] == s[right]:
+                    if right - left + 1 >= k:
+                        rst.append([left, right])
+                    left -= 1
+                    right += 1
+
+            return rst
+
+        rst = findPalindromes(s, [])
+
+        if not rst:
+            return 0
+
+        rst.sort(key=lambda x: x[1])   # 按 end 排序
+
+        ans = 0
+        endpoint = -1
+
+        for start, end in rst:
+            if start > endpoint:
+                ans += 1
+                endpoint = end
+
+        return ans

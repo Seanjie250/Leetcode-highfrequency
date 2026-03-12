2161b320fff672fde068920d14bf0e4393348192
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-12 16:21:54 -0700

    Time: 0 ms (100%), Space: 19.2 MB (96.68%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,21 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        rst = []
-        left = 0
-        total = 0
-        i , j = len(a) - 1 , len(b) - 1
-        while i >= 0 or j >= 0 or left == 1:
-            total = left
-            if i >= 0 :
-                total += int(a[i])
-                i -= 1
-            if j >= 0:
-                total += int(b[j])
-                j -= 1
-            rst.append(str(total % 2))
-            left = total // 2
-        
-        return ''.join(rst[::-1])
-
-
+        val = int(a, 2) + int(b , 2)
+        print(val)
+        return format(val , 'b')
         

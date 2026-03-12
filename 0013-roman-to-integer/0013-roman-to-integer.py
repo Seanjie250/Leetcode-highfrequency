fb411d1f25ef611d297cb74c47bc496123d8816b
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-12 13:11:19 -0700

    Time: 8 ms (26.49%), Space: 19.3 MB (85.07%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,31 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        val = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000,
-            'IV':4,
-            'IX':9,
-            'XL':40,
-            'XC':90,
-            'CD':400,
-            'CM':900
-        }
+        mapping = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+            }
         rst = 0
-        i = 0
-        while i < len(s):
-            if i + 1 < len(s) and s[i:i + 2] in val:
-                rst += val[s[i:i + 2]]
-                i += 2
+        for i in range(len(s)):
+            if i + 1 < len(s) and mapping[s[i]] < mapping[s[i + 1]]:
+                rst -= mapping[s[i]]
             else:
-                rst += val[s[i]]
-                i += 1
+                rst += mapping[s[i]]
         return rst
-      
-            
-            
         

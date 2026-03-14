d70c8b5599808e74af01ca0b9d1d237b389daf1c
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-13 21:03:15 -0700

    Time: 39 ms (17.68%), Space: 27.5 MB (26.65%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_time = [0] * len(nums)
-        right_time = [0] * len(nums)
-        rst = [0] * len(nums)
-        left_time[0] = nums[0]
-        right_time[- 1] = nums[-1]
+        fix_l = [0] * len(nums)
+        fix_l[0] = nums[0]
+        fix_r = [0] * len(nums)
+        fix_r[-1] = nums[-1]
         for i in range(1, len(nums)):
-            left_time[i] = nums[i] * left_time[i - 1]
-        for i in range(len(nums) - 2 , - 1, - 1):
-            right_time[i] = nums[i] * right_time[i + 1]
-
-        rst[0] = right_time[1]
-        rst[-1] = left_time[-2]
-
+            fix_l[i] = fix_l[i - 1] * nums[i]     
+        for i in range(len(nums) - 2 , -1 , -1):
+            fix_r[i] = fix_r[i + 1] * nums[i] 
+        rst = [0] * len(nums)
+        rst[0] = fix_r[1]
+        rst[-1] = fix_l[-2]
         for i in range(1 , len(nums) - 1):
-            rst[i] = left_time[i - 1] * right_time[i + 1]
+            rst[i] = fix_l[i - 1] * fix_r[i + 1]
         return rst
-
-
-        
-
-
-            
-
-        
-        

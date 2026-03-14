dc8215f062f1a27da1d8d58a317f433d841f66af
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-14 12:50:33 -0700

    Time: 0 ms (100%), Space: 19.3 MB (66.54%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,34 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m , n = len(matrix) , len(matrix[0])
-        l_b , r_b = 0 , n - 1
-        t_b , b_b = 0 , m - 1
         rst = []
+        l_b , r_b , t_b , b_b = 0 , n - 1 , 0 , m - 1
+       
         while l_b <= r_b and t_b <= b_b:
+            for i in range(l_b ,r_b + 1):
+                rst.append(matrix[t_b][i])
+            t_b += 1
+            for i in range(t_b , b_b + 1):
+                rst.append(matrix[i][r_b])
+            r_b -= 1
             if t_b <= b_b:
-
-                for i in range(l_b , r_b + 1):
-                    rst.append(matrix[t_b][i])
-                t_b += 1
-            
-            if r_b >= l_b:
-
-                for i in range(t_b , b_b + 1):
-                    rst.append(matrix[i][r_b])
-                r_b -= 1
-
-            if t_b <= b_b:
-
-                for i in range(r_b , l_b - 1,  - 1):
+                for i in range(r_b ,l_b - 1, - 1):
                     rst.append(matrix[b_b][i])
                 b_b -= 1
-            
-            if r_b >= l_b:
-
-                for i in range(b_b  , t_b - 1 , - 1):
+            if l_b <= r_b:
+                for i in range(b_b , t_b - 1 , - 1):
                     rst.append(matrix[i][l_b])
                 l_b += 1
         return rst
-            
-                    
+        
         

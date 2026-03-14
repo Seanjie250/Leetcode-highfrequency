94003c1f8a35940cb594d1c4edfcf0eca1281091
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-14 15:25:10 -0700

    Time: 0 ms (100%), Space: 19.4 MB (65.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,22 +2,22 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         path = []
         rst = []
-        open_c , close_c = n , n
+        open_c , close_c = 0 , 0
         def backtracking(open_c , close_c , path):
-            if close_c < 0 or open_c < 0:
+            if close_c > n or open_c > n:
                 return
-            if close_c < open_c:
+            if close_c > open_c:
                 return
-            if close_c == 0 and open_c == 0:
+            if close_c == n and open_c == n:
                 rst.append(''.join(path))
             path.append('(')
-            backtracking(open_c - 1 , close_c  , path)
+            backtracking(open_c + 1 , close_c  , path)
             path.pop()
             path.append(')')
-            backtracking(open_c , close_c -1 , path)
+            backtracking(open_c , close_c + 1, path)
             path.pop()
             return rst
-        rst = backtracking(n , n , [])
+        rst = backtracking(0 , 0 , [])
         return rst
 
                          

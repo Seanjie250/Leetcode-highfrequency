c642c1dd80cea8bc8884efc44227041104644018
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-19 12:33:42 -0700

    Time: 761 ms (25.43%), Space: 23.1 MB (11.44%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,34 @@
 class Solution:
-    def twoSum(self, nums , target):
-        rst = []
+    def twoSum(self , nums , target):
+        rst  = []
         left , right = 0 , len(nums) - 1
         while left < right:
-            if nums[left] + nums[right] > target:
-                right -= 1
-            elif nums[left] + nums[right] < target:
+            if nums[left] + nums[right] == target:
+                rst.append([nums[left] , nums[right]])
+                print(rst)
                 left += 1
-            else:
-                rst.append((nums[left] , nums[right]))
-                while left < right and  nums[left] == nums[left + 1]:
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
                     left += 1
-                while left < right and  nums[right] == nums[right - 1]:
+                while left < right and nums[right] == nums[right + 1]:
                     right -= 1
-                left += 1
+            elif nums[left] + nums[right] > target:
                 right -= 1
+            else:
+                left += 1
         return rst
+
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums = sorted(nums)
         rst = []
-        for i in range(len(nums) - 2):
+        seen = set()
+        n = len(nums)
+        for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            target = -nums[i]
-            for a , b  in self.twoSum(nums[i + 1:] , target):
-                rst.append([nums[i] , a , b ])
-    
+            target = - nums[i]
+            subset = self.twoSum(nums[i + 1 : ] , target)
+            for sub in subset:
+                rst.append([nums[i]] + sub)
         return rst
 
-        

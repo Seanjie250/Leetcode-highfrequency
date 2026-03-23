ebe78f63630dfade8bd7f728259ff70126243c71
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-23 13:11:24 -0700

    Time: 3 ms (87.57%), Space: 20.5 MB (62.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        indegree = [0] * numCourses
-        for course , prerequisite in prerequisites:
-            graph[prerequisite].append(course)
-            indegree[course] += 1
-        
-        count = 0
-
+        degree = [0] * numCourses
+        for c , p in prerequisites:
+            graph[p].append(c)
+            degree[c] += 1
         q = deque()
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
+        for i in range(len(degree)):
+            if degree[i] == 0:
                 q.append(i)
-            
+        count = 0
         while q:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 cur = q.popleft()
                 count += 1
-                for course in graph[cur]:
-                    indegree[course] -= 1
-                    if indegree[course] == 0:
-                        q.append(course)
+                for co in graph[cur]:
+                    degree[co] -= 1
+                    if degree[co] == 0:
+                        q.append(co)
+        print(count)
         return count == numCourses
+
 
         

8266cbc46948ba473a0e91ffdb47e0bfe48b1afd
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-23 12:40:42 -0700

    Time: 98 ms (87.65%), Space: 77.3 MB (35.21%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,40 +1,37 @@
 class ListNode:
-    def __init__(self, key , val):
+    def __init__(self , key , val):
         self.key = key
         self.val = val
-        self.next = None
         self.prev = None
+        self.next = None
 class LRUCache:
     def __init__(self, capacity: int):
-        self.head = ListNode(0, 0)
-        self.tail = ListNode(0 ,0)
-        self.cache = {}
-        self.capacity = capacity
+        self.head = ListNode(-1 , -1)
+        self.tail = ListNode(-1 , -1)
         self.head.next = self.tail
         self.tail.prev = self.head
-    
+        self.cache = {}
+        self.capacity = capacity
     def remove(self , node):
         nxt = node.next
         prv = node.prev
-        nxt.prev = prv
         prv.next = nxt
-
-    def add_to_head(self , node):
+        nxt.prev = prv
+    def add_to_head(self, node):
         nxt = self.head.next
         node.next = nxt
         node.prev = self.head
         self.head.next = node
+        node.prev = self.head
         nxt.prev = node
-
     def get(self, key: int) -> int:
         if not key in self.cache:
             return -1
         node = self.cache[key]
-        val = node.val
+        value = node.val
         self.remove(node)
         self.add_to_head(node)
-        return val
-
+        return value
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
@@ -42,14 +39,16 @@ class LRUCache:
             self.remove(node)
             self.add_to_head(node)
         else:
-            while len(self.cache) >= self.capacity:
-                tail = self.tail.prev
-                key_tail = tail.key
-                self.remove(tail)
-                del self.cache[key_tail]
             node = ListNode(key , value)
-            self.cache[key] = node
             self.add_to_head(node)
+            self.cache[key] = node
+            if len(self.cache) > self.capacity:
+                tail_node = self.tail.prev
+                tail_key = tail_node.key
+                self.remove(tail_node)
+                del self.cache[tail_key]
+
+        
         
 
 

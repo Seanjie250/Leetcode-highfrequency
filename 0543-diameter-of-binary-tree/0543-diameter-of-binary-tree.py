7886d04141a1ca7d51bedefada5f5632779283d2
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-03-30 13:23:34 -0700

    Time: 7 ms (22.54%), Space: 22 MB (93.52%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,8 +1,25 @@
 class TreeNode:
-    def __init__(self , left , right , val):
+    def __init__(self , val):
         self.left = None
         self.right = None
         self.val = val
+def tree_build(arr):
+    if not arr or arr[0] == - 1:
+        return None
+    root = TreeNode(arr[0])
+    queue = [root]
+    i = 1
+    while queue and i < len(arr):
+        node = queue.pop(0)
+        if queue and arr[i] != -1:
+            node.left = TreeNode(arr[i])
+            queue.append(node.left)
+        i += 1
+        if queue and arr[i] != -1:
+            node.right = TreeNode(arr[i])
+            queue.append(node.right)
+        i += 1
+    return root
 class Solution:
     def diameterOfBinaryTree(self , root):
         self.maxm = float('-inf')
@@ -15,4 +32,8 @@ class Solution:
             return 1 + max(left , right)
         dfs(root)
         return self.maxm
-        
+    
+arr = [1, 2, 3, 4, 5]
+root = tree_build(arr)
+ans = Solution().diameterOfBinaryTree(root)
+print(ans)

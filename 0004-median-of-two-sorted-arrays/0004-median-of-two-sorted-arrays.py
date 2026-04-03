dd8172b2f9e7bd1d7b614e39456edcafd85240f9
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-04-03 13:17:29 -0700

    Time: 1 ms (58.2%), Space: 19.5 MB (59.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,27 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
-       
-        if len(nums1)> len(nums2):
+        if len(nums1) > len(nums2):
             return self.findMedianSortedArrays(nums2 , nums1)
-        m , n = len(nums1) , len(nums2)
-        left , right = 0 , m
-        while left <= right:
-            part1 = (left + right) // 2
-            part2 = (m + n + 1) // 2 - part1
-
-            max_left1 = float('-inf') if part1 == 0 else nums1[part1 - 1]
-            min_right1 = float('inf') if part1 == m else nums1[part1]
-            max_left2 = float('-inf') if part2 == 0 else nums2[part2 - 1]
-            min_right2 = float('inf') if part2 == n else nums2[part2]
-
-            if max_left1 <= min_right2 and min_right1 >= max_left2:
-                if (m + n) % 2 == 0:
-                    return (max(max_left1 , max_left2) + min(min_right1 , min_right2)) / 2
+        l , r = 0 , len(nums1)
+        while l <= r:
+            pA = (l + r) // 2
+            pB = (len(nums1) + len(nums2) + 1) // 2 - pA
+            leftmaxA = nums1[pA - 1] if pA > 0 else float('-inf')
+            rightminA = nums1[pA] if pA < len(nums1) else float('inf')
+            leftmaxB = nums2[pB - 1] if pB > 0 else float('-inf')
+            rightminB = nums2[pB] if pB < len(nums2) else float('inf')
+            
+            if leftmaxA <= rightminB and rightminA >= leftmaxB:
+                if (len(nums1) + len(nums2)) % 2:
+                    return float(max(leftmaxA  , leftmaxB))
                 else:
-                    return max(max_left1, max_left2)
-            elif max_left1 > min_right2:
-                right = part1 - 1
+                    return (max(leftmaxA  , leftmaxB) + min(rightminA , rightminB)) / 2.0
             else:
-                left = part1 + 1
+                if leftmaxA > rightminB:
+                    r = pA - 1
+                elif rightminA < leftmaxB:
+                    l = pA + 1
+            
+
         

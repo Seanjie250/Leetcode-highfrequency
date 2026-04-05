324e79e6fb17333b70ec3cdeb8b3a294e46ff816
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-04-05 15:02:11 -0700

    Time: 3 ms (3.7%), Space: 19.4 MB (56.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,25 +1,29 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        graph = {
-            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        if not digits:
+            return []
+        counter = {
+            '2' : 'abc',
+            '3' : 'def',
+            '4' : 'ghi',
+            '5' : 'jkl',
+            '6' : 'mno',
+            '7' : 'pqrs',
+            '8' : 'tuv',
+            '9' : 'wxyz'
         }
-        rst = []
-        path = []
-        def dfs(i):
-            if i == len(digits):
+        def backtracking(path , i , rst):
+            if len(path) == len(digits):
                 rst.append(''.join(path))
-                return
-            for ch in graph[digits[i]]:
-                path.append(ch)
-                dfs(i + 1)
-                path.pop()
-        
-        dfs(0)
+            if i < len(digits):
+                for ch in counter[digits[i]]:
+                    path.append(ch)
+                    backtracking(path , i + 1 , rst)
+                    path.pop()
+
+            return rst
+        rst = backtracking([]  , 0 , [])
         return rst
 
-            
-           
-                
 
         

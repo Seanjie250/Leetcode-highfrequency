d36ca242e83313100a56aac4b3e590b682e0a0d1
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-04-05 17:11:40 -0700

    Time: 3 ms (88.74%), Space: 20.1 MB (77.42%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,22 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        ans = 0
-        cur_pos = 0
-        next_pos = 0
-        if len(nums) == 1:
-            return 0
-        for i in range(len(nums)):
-            next_pos = max(next_pos , nums[i] + i)
-            if i == cur_pos:
-                ans += 1
-                cur_pos = next_pos
-            if cur_pos >= len(nums) - 1:
-                break
-        return ans
-
- 
-
-
-
-
+        far = 0
+        end = 0
+        rst = 0
+        for i in range(len(nums) - 1):
+            far = max(i + nums[i] , far)
+            if i == end:
+                end = far
+                rst += 1
+        return rst
+        
+        
         

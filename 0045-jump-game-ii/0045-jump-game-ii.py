74a2740643029333860eaac2d7742fbcf2380c00
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-04-06 19:32:38 -0700

    Time: 7 ms (53.19%), Space: 20.1 MB (77.06%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        far = 0
         end = 0
-        rst = 0
+        far = 0
+        count = 0
         for i in range(len(nums) - 1):
-            far = max(i + nums[i] , far)
+            far = max(far , i + nums[i])
+            
             if i == end:
                 end = far
-                rst += 1
-        return rst
-        
-        
+                count += 1
+        return count
+
         

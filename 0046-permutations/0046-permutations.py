5fa5e36d5768695cdf7fa8f52a64d51675c6bc45
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-04-07 18:09:54 -0700

    Time: 0 ms (100%), Space: 19.7 MB (18.98%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,24 +1,19 @@
 class Solution:
-        def permute(self, nums: List[int]) -> List[List[int]]:
-        
-            path = []
-            rst = []
-            visited = [False] * len(nums)
-            def backtracking(path , rst):
-                if len(path) == len(nums):
-                    rst.append(list(path))
-                    return
-                for i in range(len(nums)):
-                    if not visited[i]:
-                        visited[i] = True
-                        path.append(nums[i])
-                        backtracking(path , rst)
-                        path.pop()
-                        visited[i] = False
-            backtracking(path ,rst)
-            return rst
-
-            
-
-        
-        
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        rst = []
+        path = []
+        visited = set()
+        def dfs(path):
+            if len(path) == len(nums):
+                rst.append(path[:])
+            for num in nums:
+                if num not in visited:
+                    visited.add(num)
+                    path.append(num)
+                    
+                    dfs(path)
+                    path.pop()
+                    visited.remove(num)
+        dfs(path)
+        return rst
+                    

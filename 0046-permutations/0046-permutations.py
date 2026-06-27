535856bed12f63de9404c8b9907f7a0b1b97bdbe
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-06-27 14:21:19 -0700

    Time: 6 ms (1.77%), Space: 19.3 MB (86.68%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,21 @@
 class Solution:
+    def dfs(self , path , rst , nums , visited):
+        if len(path) == len(nums) and path not in rst:
+            rst.append(path[:])
+        for num in nums:
+            if num not in visited:
+                visited.add(num)
+                path.append(num)
+                self.dfs(path , rst,  nums , visited)
+                path.pop()
+                visited.remove(num)
+        return rst
     def permute(self, nums: List[int]) -> List[List[int]]:
         rst = []
         path = []
         visited = set()
-        def dfs(path):
-            if len(path) == len(nums):
-                rst.append(path[:])
-            for num in nums:
-                if num not in visited:
-                    visited.add(num)
-                    path.append(num)
-                    
-                    dfs(path)
-                    path.pop()
-                    visited.remove(num)
-        dfs(path)
+        rst = self.dfs(path , rst,  nums ,visited)
         return rst
-                    
+
+        
+        

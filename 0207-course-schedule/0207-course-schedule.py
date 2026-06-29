5684fb592afd1e5beae8df7462a4290fdc6f9249
Zhengjie(Sean) Sun <120350587+Seanjie250@users.noreply.github.com>
2026-06-29 10:39:26 -0700

    Time: 7 ms (46.05%), Space: 20.3 MB (79.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,25 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
+        graph = [[] for _ in range(numCourses)]
         degree = [0] * numCourses
-        for c , p in prerequisites:
-            graph[p].append(c)
-            degree[c] += 1
-        q = deque()
-        for i in range(len(degree)):
-            if degree[i] == 0:
-                q.append(i)
+        for course , pre in prerequisites:
+            graph[pre].append(course)
+            degree[course] += 1
+        print(graph)
+        print(degree)
+
+        queue = deque()
         count = 0
-        while q:
-            for _ in range(len(q)):
-                cur = q.popleft()
+
+        for i in range(numCourses):
+            if degree[i] == 0:
+                queue.append(i)
+        if not queue:
+            return False
+        while queue:
+            for i in range(len(queue)):
+                cur_course = queue.popleft()
+                for course in graph[cur_course]:
+                    degree[course] -= 1
+                    if degree[course] == 0:
+                        queue.append(course)
                 count += 1
-                for co in graph[cur]:
-                    degree[co] -= 1
-                    if degree[co] == 0:
-                        q.append(co)
-        print(count)
+
+            
         return count == numCourses
-
-
         
